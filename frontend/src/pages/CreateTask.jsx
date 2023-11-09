dfef082acf022bdad04a1b898b6ee4146cf898e3
cabbage <shakun650@gmail.com>
2023-11-09 17:24:28 +0600

    Personal modifications

--- frontend/src/pages/CreateTask.jsx
+++ frontend/src/pages/CreateTask.jsx
@@ -1,182 +1,240 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import style from "../style/CreateTask.module.css";
-import { useLocation } from "react-router-dom";
 
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import UserApi from "../api/UserApi";
-import { useState } from "react";
 import { formatDateAndTime } from "../utilities/formatDate";
 export default function CreateTask() {
-    const location = useLocation();
-    const { editMode } = location.state || false;
-    const { task } = location.state || false;
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  const { editMode } = location.state || false;
+  const { task } = location.state || false;
 
-    const [title, setTitle] = useState(false);
-    const [description, setDescription] = useState(false);
-    const [category, setCategory] = useState(false);
-    const [dueDate, setDueDate] = useState(false);
-    const [priority, setPriority] = useState(false);
+  const [title, setTitle] = useState(false);
+  const [description, setDescription] = useState(false);
+  const [category, setCategory] = useState(false);
+  const [dueDate, setDueDate] = useState(false);
+  const [priority, setPriority] = useState(false);
 
-    async function handleAddTask(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const formDataObject = Object.fromEntries(formData);
-        console.log("Form Data Example : ", formDataObject);
-        formDataObject["priority"] = parseInt(formDataObject["priority"]);
+  const navigate = useNavigate();
 
-        try {
-            //
-            const response = await UserApi.post(
-                "/create-task",
-                formDataObject,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
+  useEffect(() => {
+    toast.onChange((payload) => {
+      if (payload.status === "removed") {
+        // Refresh the page
+        navigate("/");
+      }
+    });
+  }, [navigate]);
 
-            console.log(response.data);
-        } catch (err) {
-            //
-            console.log(err);
-        }
+  async function handleAddTask(e) {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const formDataObject = Object.fromEntries(formData);
+    console.log("Form Data Example : ", formDataObject);
+    formDataObject["priority"] = parseInt(formDataObject["priority"]);
+
+    try {
+      //
+      const response = await UserApi.post("/create-task", formDataObject, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      toast.success("Task added successfully...", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1200,
+      });
+      setLoading(false);
+      console.log(response.data);
+    } catch (err) {
+      setError(err.response.data.error);
+      setLoading(false);
+      //
+      console.log(err);
     }
-    async function handleUpdateTask(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const formDataObject = Object.fromEntries(formData);
+  }
+  async function handleUpdateTask(e) {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const formDataObject = Object.fromEntries(formData);
 
-        formDataObject["task_id"] = task._id;
-        formDataObject["priority"] = parseInt(formDataObject["priority"]);
-        console.log("Form Data Example : ", formDataObject);
+    formDataObject["task_id"] = task._id;
+    formDataObject["priority"] = parseInt(formDataObject["priority"]);
+    console.log("Form Data Example : ", formDataObject);
 
-        try {
-            //
-            const response = await UserApi.put("/update-task", formDataObject, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+    try {
+      //
+      const response = await UserApi.put("/update-task", formDataObject, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      toast.success("Task updated successfully...", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1200,
+      });
+      setLoading(false);
 
-            console.log(response.data);
-        } catch (err) {
-            //
-            console.log(err);
-        }
+      console.log(response.data);
+    } catch (err) {
+      setError(err.response.data.error);
+      setLoading(false);
+      console.log(err);
     }
+  }
 
-    return (
-        <div>
-            <h2>{editMode ? `Edit Form` : `Task Creation Form`}</h2>
-            <form
-                onSubmit={!editMode ? handleAddTask : handleUpdateTask}
-                className={style["task-form"]}
+  return (
+    <>
+      <div>
+        <h2 style={{ fontSize: "35px", color: "#000" }}>
+          {editMode ? `Edit Form` : `Task Creation Form`}
+        </h2>
+        <form
+          onSubmit={!editMode ? handleAddTask : handleUpdateTask}
+          className={style["task-form"]}
+        >
+          <div>
+            <label htmlFor="title" style={{ fontSize: "25px", color: "#000" }}>
+              Task Title
+            </label>
+            <input
+              required
+              type="text"
+              name="title"
+              id="title"
+              value={
+                editMode && title === false
+                  ? task.title
+                  : title === false
+                  ? ""
+                  : title
+              }
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="description"
+              style={{ fontSize: "25px", color: "#000" }}
             >
-                <div>
-                    <label htmlFor="title">Task Title</label>
-                    <input
-                        type="text"
-                        name="title"
-                        id="title"
-                        value={
-                            editMode && title === false
-                                ? task.title
-                                : title === false
-                                ? ""
-                                : title
-                        }
-                        onChange={(e) => {
-                            setTitle(e.target.value);
-                        }}
-                    />
-                </div>
+              Description
+            </label>
+            <textarea
+              required
+              name="description"
+              id="description"
+              cols="30"
+              rows="10"
+              value={
+                editMode && description === false
+                  ? task.description
+                  : description === false
+                  ? ""
+                  : description
+              }
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
+          </div>
 
-                <div>
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                        name="description"
-                        id="description"
-                        cols="30"
-                        rows="10"
-                        value={
-                            editMode && description === false
-                                ? task.description
-                                : description === false
-                                ? ""
-                                : description
-                        }
-                        onChange={(e) => {
-                            setDescription(e.target.value);
-                        }}
-                    ></textarea>
-                </div>
+          <div>
+            <label
+              htmlFor="category"
+              style={{ fontSize: "25px", color: "#000" }}
+            >
+              Category
+            </label>
+            <input
+              required
+              type="text"
+              name="categories"
+              id="category"
+              placeholder="Seperate categories with comma (eg. CatA,CatB)"
+              value={
+                editMode && category === false
+                  ? task.categories?.map((category) => category).join(", ")
+                  : category === false
+                  ? ""
+                  : category
+              }
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="title" style={{ fontSize: "25px", color: "#000" }}>
+              Due Date
+            </label>
+            <input
+              required
+              type="date"
+              name="due_date"
+              id=""
+              value={
+                editMode && dueDate === false
+                  ? formatDateAndTime(task.dueDate).date
+                  : dueDate === false
+                  ? ""
+                  : dueDate
+              }
+              onChange={(e) => {
+                setDueDate(e.target.value);
+              }}
+            />
+          </div>
 
-                <div>
-                    <label htmlFor="category">Category</label>
-                    <input
-                        type="text"
-                        name="categories"
-                        id="category"
-                        placeholder="comma separated"
-                        value={
-                            editMode && category === false
-                                ? task.categories
-                                      ?.map((category) => category)
-                                      .join(", ")
-                                : category === false
-                                ? ""
-                                : category
-                        }
-                        onChange={(e) => {
-                            setCategory(e.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="title">Due Data</label>
-                    <input
-                        type="date"
-                        name="due_date"
-                        id=""
-                        value={
-                            editMode && dueDate === false
-                                ? formatDateAndTime(task.dueDate).date
-                                : dueDate === false
-                                ? ""
-                                : dueDate
-                        }
-                        onChange={(e) => {
-                            setDueDate(e.target.value);
-                        }}
-                    />
-                </div>
+          <div>
+            <label
+              htmlFor="priority"
+              style={{ fontSize: "25px", color: "#000" }}
+            >
+              Priority
+            </label>
+            <select
+              name="priority"
+              id="priority"
+              value={
+                editMode && priority === false
+                  ? task.priority
+                  : priority === false
+                  ? ""
+                  : priority
+              }
+              onChange={(e) => {
+                setPriority(e.target.value);
+              }}
+            >
+              <option value={1}>High</option>
+              <option value={2}>Medium</option>
+              <option value={3}>Low</option>
+            </select>
+          </div>
 
-                <div>
-                    <label htmlFor="priority">Priority</label>
-                    <select
-                        name="priority"
-                        id="priority"
-                        value={
-                            editMode && priority === false
-                                ? task.priority
-                                : priority === false
-                                ? ""
-                                : priority
-                        }
-                        onChange={(e) => {
-                            setPriority(e.target.value);
-                        }}
-                    >
-                        <option value={1}>High</option>
-                        <option value={2}>Medium</option>
-                        <option value={3}>Low</option>
-                    </select>
-                </div>
-
-                <input
-                    type="submit"
-                    value={editMode ? "Update Task" : "Add Task"}
-                />
-            </form>
-        </div>
-    );
+          <input
+            disabled={loading}
+            type="submit"
+            value={editMode ? "Update Task" : "Add Task"}
+            style={{
+              backgroundColor: "lightgreen",
+              fontSize: "20px",
+            }}
+          />
+        </form>
+      </div>{" "}
+      <ToastContainer position="top-right" />
+    </>
+  );
 }
--- frontend/src/pages/TaskDetails.jsx
+++ frontend/src/pages/TaskDetails.jsx
@@ -1,129 +1,153 @@
-import { useState } from "react";
-import style from "../style/TaskDetails.module.css";
-import { Link, useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import UserApi from "../api/UserApi";
+import style from "../style/TaskDetails.module.css";
 import { formatDateAndTime } from "../utilities/formatDate";
+
 export default function TaskDetails() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const location = useLocation();
-    const { task } = location.state;
-    const { title, description, dueDate, priority, categories, completed } =
-        task;
-    const [completeStatus, setCompleteStatus] = useState(completed);
-    //     {
-    //     "_id": "654c89b5ccd284640d863930",
-    //     "title": "abdfds",
-    //     "description": "dsfsdfsd",
-    //     "dueDate": "2023-11-10T00:00:00.000Z",
-    //     "priority": 2,
-    //     "completed": false,
-    //     "categories": [
-    //         "sdf",
-    //         "vdfgd"
-    //     ],
-    //     "__v": 0
-    // }
-    console.log("TASK DETAILS :", task);
+  const location = useLocation();
+  const { task } = location.state;
+  const { title, description, dueDate, priority, categories, completed } = task;
+  const [completeStatus, setCompleteStatus] = useState(completed);
+  //     {
+  //     "_id": "654c89b5ccd284640d863930",
+  //     "title": "abdfds",
+  //     "description": "dsfsdfsd",
+  //     "dueDate": "2023-11-10T00:00:00.000Z",
+  //     "priority": 2,
+  //     "completed": false,
+  //     "categories": [
+  //         "sdf",
+  //         "vdfgd"
+  //     ],
+  //     "__v": 0
+  // }
+  console.log("TASK DETAILS :", task);
 
-    async function markAsDone() {
-        const categories = task.categories
-            ?.map((category) => category)
-            .join(",");
-        const dueDate = formatDateAndTime(task.dueDate).date;
-        const tempObj = {
-            ...task,
-            completed: 1,
-            task_id: task._id,
-            categories,
-            due_date: dueDate,
-        };
-        console.log("Temp obj", tempObj);
-        try {
-            //
-            const response = await UserApi.put("/update-task", tempObj, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+  useEffect(() => {
+    toast.onChange((payload) => {
+      if (payload.status === "removed") {
+        // Refresh the page
+        navigate("/");
+      }
+    });
+  }, [navigate]);
 
-            console.log(response.data);
-            setCompleteStatus(true);
-        } catch (err) {
-            //
-            console.log(err);
-        }
+  async function markAsDone() {
+    setError(false);
+    setLoading(true);
+    const categories = task.categories?.map((category) => category).join(",");
+    const dueDate = formatDateAndTime(task.dueDate).date;
+    const tempObj = {
+      ...task,
+      completed: 1,
+      task_id: task._id,
+      categories,
+      due_date: dueDate,
+    };
+    console.log("Temp obj", tempObj);
+    try {
+      //
+      const response = await UserApi.put("/update-task", tempObj, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setLoading(false);
+
+      toast.success("Task completed..", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1200, // Time in milliseconds to auto-close the toast (1.5 seconds in this case)
+      });
+
+      console.log(response.data);
+      setCompleteStatus(true);
+    } catch (err) {
+      setError(err.response.data.error);
+      setLoading(false);
+      console.log(err);
     }
-    async function deleteTask() {
-        try {
-            //
-            const response = await UserApi.post(
-                "/delete-task",
-                { task_id: task._id },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-
-            console.log(response.data);
-            navigate("/");
-        } catch (err) {
-            //
-            console.log(err);
+  }
+  async function deleteTask() {
+    setError(false);
+    setLoading(true);
+    try {
+      //
+      const response = await UserApi.post(
+        "/delete-task",
+        { task_id: task._id },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      setLoading(false);
+
+      toast.success("Task completed..", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1200, // Time in milliseconds to auto-close the toast (1.5 seconds in this case)
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      setError(err.response.data.error);
+      setLoading(false);
+      console.log(err);
     }
+  }
 
-    return (
-        <>
-            <div className={style["task-details-page"]}>
-                <h2>{title}</h2>
-                <p>
-                    <b>Task Description: </b>
-                    {description}
-                </p>
-                <p>
-                    <b>Due Date: </b>
-                    {formatDateAndTime(dueDate).date}
-                </p>
-                <p>
-                    <b>Priority</b>
-                    {priority}
-                </p>
-                <p>
-                    <b>Categories: </b>
-                    {categories?.map((category) => category).join(", ")}
-                </p>
-                <p>
-                    <b>Status: </b>
-                    {completeStatus ? "Done" : "Pending..."}
-                </p>
-            </div>
+  return (
+    <>
+      <div className={style["task-details-page"]}>
+        <h2 style={{ fontSize: "35px", color: "#000" }}>{title}</h2>
+        <p style={{ fontSize: "25px", color: "#000", marginBottom: "10px" }}>
+          <b>Task Description: </b>
+          {description}
+        </p>
+        <p style={{ fontSize: "25px", color: "#000", marginBottom: "10px" }}>
+          <b>Due Date: </b>
+          {formatDateAndTime(dueDate).date}
+        </p>
+        <p style={{ fontSize: "25px", color: "#000", marginBottom: "10px" }}>
+          <b>Priority: </b>
+          {priority}
+        </p>
+        <p style={{ fontSize: "25px", color: "#000", marginBottom: "10px" }}>
+          <b>Categories: </b>
+          {categories?.map((category) => category).join(", ")}
+        </p>
+        <p style={{ fontSize: "25px", color: "#001", marginBottom: "10px" }}>
+          <b>Status: </b>
+          {completeStatus ? "Done" : "Pending..."}
+        </p>
+      </div>
 
-            <div className={style["details-button"]}>
-                <Link
-                    to="/create-task"
-                    state={{
-                        editMode: true,
-                        task,
-                    }}
-                    className="btn"
-                >
-                    Edit Details
-                </Link>
-                <button
-                    className="btn"
-                    onClick={markAsDone}
-                    disabled={completeStatus}
-                >
-                    {completeStatus ? "Already Done" : "Mark As Done"}
-                </button>
-                <button onClick={deleteTask} className="btn">
-                    Delete
-                </button>
-            </div>
-        </>
-    );
+      <div className={style["details-button"]}>
+        <Link
+          to="/create-task"
+          state={{
+            editMode: true,
+            task,
+          }}
+          className="btn"
+        >
+          Edit Details
+        </Link>
+        <button className="btn" onClick={markAsDone} disabled={completeStatus}>
+          {completeStatus ? "Already Done" : "Mark As Done"}
+        </button>
+        <button onClick={deleteTask} className="btn">
+          Delete
+        </button>
+      </div>
+      <ToastContainer position="top-right" />
+    </>
+  );
 }
